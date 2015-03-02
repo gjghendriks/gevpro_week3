fd4092705913a859d3cc7a2c0c44b49740785df5
gjghendriks <gjghendriks@gmail.com>
2015-03-02 11:48:26 +0100

    finished exercise

--- spontal_filter.py
+++ spontal_filter.py
@@ -1,1 +1,33 @@
-print('hello')
+import xml.etree.ElementTree as ET
+import sys
+tree = ET.parse(sys.argv[1])
+root = tree.getroot()
+spontal = 3
+spontal_filtered = 3
+for child in root:
+    spontal += 2
+    for nested in child:
+        spontal += 1
+
+for point in root:
+    
+    if(point.find('BOTTOM_HZ').text > point.find('F0_START').text > point.find('TOP_HZ').text):
+        #print(point.find('BOTTOM_HZ').text, point.find('F0_END').text, point.find('F0_START').text, point.find('TOP_HZ').text)
+        root.remove(point)
+ 
+    elif(point.find('BOTTOM_HZ').text > point.find('F0_END').text > point.find('TOP_HZ').text):
+        #print(point.find('BOTTOM_HZ').text, point.find('F0_END').text, point.find('F0_START').text, point.find('TOP_HZ').text)
+        root.remove(point)
+        
+    else:
+        pass
+for child in root:
+    spontal_filtered += 2
+    for nested in child:
+        spontal_filtered += 1
+tree.write(sys.argv[2], encoding="utf-8", xml_declaration=True)
+#ET.ElementTree(root).write(sys.argv[2], encoding="utf-8", xml_declaration=True)
+print (spontal, sys.argv[1])
+print (spontal_filtered, sys.argv[2])
+print (spontal+spontal_filtered, ' total')
+
